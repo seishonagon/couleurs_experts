b08aa68c42b6cf5dc7e6443c8da4f6dbe4dc035d
Paul Atlan <paulatlan@fastmail.fm>
2022-10-15 15:12:52 +0000

    clean up

--- src/app/app.functions/site-card.js
+++ src/app/app.functions/site-card.js
@@ -6,26 +6,6 @@ const hubspot = require('@hubspot/api-client');
 exports.main = async (context = {}, sendResponse) => {
   // Store contact info, configured as propertiesToSend in crm-card.json
   const { hs_object_id, firstname, lastname, type_contact } = context.propertiesToSend;
-
-  // instantiate HubSpot Node API client
-  // const hubspotClient = new hubspot.Client({
-  //   accessToken: context.secrets.PRIVATE_APP_ACCESS_TOKEN,
-  // });
-  
-  // const bandeauExpert = {
-  //   "type": "image",
-  //   "src": "https://cdn.experconnect.com/crm/expert_green.png",
-  //   "alt": "Bandeau vert expert"
-  // };
-  // const bandeauContactClient = {
-  //   "type": "image",
-  //   "src": "https://cdn.experconnect.com/crm/contact_brown.png",
-  //   "alt": "Bandeau brun contact client"
-  // };
-  // const errorMessage = {
-  //   "type": "text",
-  //   "text": `Le type contact est ${type_contact}`,
-  // };
   
   if (`${type_contact}` === `Expert`) {
     sendResponse({ sections: [{

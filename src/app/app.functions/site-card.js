d49dda5fbe9d0365aafa4b29a62559548494f2cd
Paul Atlan <paulatlan@fastmail.fm>
2022-10-15 13:30:38 +0000

    copy template over

---
+++ src/app/app.functions/site-card.js
@@ -0,0 +1,56 @@
+// Example crm-card.js file
+
+// Include HubSpot node API client
+const hubspot = require('@hubspot/api-client');
+
+exports.main = async (context = {}, sendResponse) => {
+  // Store contact info, configured as propertiesToSend in crm-card.json
+  const { hs_object_id, firstname, lastname, site, specialite_du_site } = context.propertiesToSend;
+
+  // instantiate HubSpot Node API client
+  const hubspotClient = new hubspot.Client({
+    accessToken: context.secrets.PRIVATE_APP_ACCESS_TOKEN,
+  });
+
+  // Defines the first section of the CRM card
+
+  try {
+    // Defines variables for API endpoint and response data
+    const resp = await hubspotClient.crm.contacts.basicApi.getById(hs_object_id);
+    const email = resp.properties.email
+    hubspotClient
+      .apiRequest({
+        method: 'GET',
+        path: `/crm/v4/objects/contacts/51/associations/companies`,
+      }).then(results => {
+        let objResult = results.body.results;
+        console.log(`the object is: ${objResult}`)
+      });
+
+    // const companyId = associatedCompanies.body[0].id;
+    // Defines how the returned data will be displayed
+    sendResponse({
+      sections: [{
+          type: "heading",
+          text: "Sample project custom CRM card"
+        },
+        {
+          type: "text",
+          text: `This card will retrieve data on the contact record. It is attached to contact id ${hs_object_id}.`
+        },
+        {
+          type: "text",
+          format: "markdown",
+          text: "This serverless function will retrieve a specific contact by ID."
+        },
+        {
+          type: "text",
+          format: "markdown",
+          text: `This contact's email is **${email}**`
+        },
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
